4e77ddc5ce96bfe5f46854c288eee99da97ebd0c
AngelLuna6969 <utp0149900@alumno.utpuebla.edu.mx>
2025-08-14 13:00:54 -0600

    Se agrega archivoDbContext completo

--- APISchool/Data/ApplicationDbContext.cs
+++ APISchool/Data/ApplicationDbContext.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using APISchool.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace APISchool.Data
 {
     public class ApplicationDbContext: DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+        public DbSet<MusicSchool> MusicSchools { get; set; }
+        public DbSet<Teachers> Teachers { get; set; }
+        public DbSet<Students> Students { get; set; }
+        public DbSet<TeacherStudents> TeacherStudents { get; set; }
+        
     }
 }
